28412285ada95317afdcdf1bd833a35d39eaf47a
Mateus Paulart <mateusp23@rede.ulbra.br>
2025-02-23 11:01:48 -0300

    atualizando projetos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,9 +3,49 @@ import CasaC10Image1 from "../assets/casa-c10-01.webp";
 import CasaC10Image2 from "../assets/casa-c10-02.webp";
 import CasaC10Image3 from "../assets/casa-c10-04.webp";
 
+import CasaFaroImage from "../assets/casa-del-faro.webp";
+import CasaFaroImage1 from "../assets/casa-del-faro01.webp";
+import CasaFaroImage2 from "../assets/casa-del-faro02.webp";
+import CasaFaroImage3 from "../assets/casa-del-faro03.webp";
+import CasaFaroImage4 from "../assets/casa-del-faro04.webp";
+import CasaFaroImage6 from "../assets/casa-del-faro06.webp";
+
+import CasaBellaImage from "../assets/casa-bella.webp";
+import CasaBellaImage1 from "../assets/casa-bella01.webp";
+import CasaBellaImage2 from "../assets/casa-bella02.webp";
+import CasaBellaImage3 from "../assets/casa-bella03.webp";
+import CasaBellaImage4 from "../assets/casa-bella04.webp";
+import CasaBellaImage5 from "../assets/casa-bella05.webp";
+
+import CasaJ10Image from "../assets/casa-j10.webp";
+import CasaJ10Image1 from "../assets/casa-j10-01.webp";
+import CasaJ10Image2 from "../assets/casa-j10-02.webp";
+import CasaJ10Image3 from "../assets/casa-j10-03.webp";
+import CasaJ10Image4 from "../assets/casa-j10-04.webp";
+import CasaJ10Image5 from "../assets/casa-j10-05.webp";
+import CasaJ10Image6 from "../assets/casa-j10-06.webp";
+
 const projectsData = [
   {
     id: 1,
+    slug: "bella-residencial",
+    name: "Bella Residencial",
+    year: 2021,
+    size: 360,
+    location: "Torres - RS",
+    image: CasaBellaImage, // Imagem principal
+    secondaryImage: CasaBellaImage1, // Imagem lateral
+    gallery: [
+      CasaBellaImage,
+      CasaBellaImage1,
+      CasaBellaImage2,
+      CasaBellaImage3,
+      CasaBellaImage4,
+      CasaBellaImage5
+    ]
+  },
+  {
+    id: 2,
     slug: "casa-c10",
     name: "Casa C10",
     year: 2023,
@@ -21,20 +61,39 @@ const projectsData = [
     ]
   },
   {
-    id: 2,
-    slug: "casa-rio",
-    name: "Casa Rio",
-    year: 2023,
+    id: 3,
+    slug: "casa-del-faro",
+    name: "Casa Del Faro",
+    year: 2024,
     size: 300,
     location: "Torres - RS",
-    image: "/images/casa-rio/main.jpg",
-    secondaryImage: "/images/casa-rio/secondary.jpg",
+    image: CasaFaroImage,
+    secondaryImage: CasaFaroImage1,
     gallery: [
-      "/images/casa-rio/1.jpg",
-      "/images/casa-rio/2.jpg",
-      "/images/casa-rio/3.jpg"
+      CasaFaroImage2,
+      CasaFaroImage3,
+      CasaFaroImage4,
+      CasaFaroImage6
     ]
-  }
+  },
+  {
+    id: 4,
+    slug: "casa-j10",
+    name: "Casa J10",
+    year: 2022,
+    size: 188.78,
+    location: "Torres - RS",
+    image: CasaJ10Image,
+    secondaryImage: CasaJ10Image1,
+    gallery: [
+      CasaJ10Image2,
+      CasaJ10Image3,
+      CasaJ10Image4,
+      CasaJ10Image5,
+      CasaJ10Image6,
+      CasaJ10Image
+    ]
+  },
 ];
 
 export default projectsData;
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,40 +1,49 @@
 import PortfolioHeader from "../components/PortfolioHeader";
 import ProjectCard from "../components/ProjectCard";
 import CasaC10Image from "../assets/casa-c10-03.webp";
+import CasaFaroImage from "../assets/casa-del-faro.webp";
+import CasaBellaImage from "../assets/casa-bella.webp";
+import CasaJ10Image from "../assets/casa-j10-03.webp";
 
 const projectsData = [
   {
     id: 1,
+    name: "Bella Residencial",
+    slug: "bella-residencial",
+    image: CasaBellaImage,
+  },
+  {
+    id: 2,
     name: "Casa C10",
     slug: "casa-c10",
     image: CasaC10Image,
   },
   {
-    id: 2,
-    name: "Residência Horizonte",
-    slug: "residencia-horizonte",
-    image: "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWV8ZW58MHx8MHx8fDA%3D",
-  },
-  {
     id: 3,
-    name: "Loft Industrial",
-    slug: "loft-industrial",
-    image: "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWV8ZW58MHx8MHx8fDA%3D",
+    name: "Casa Del Faro",
+    slug: "casa-del-faro",
+    image: CasaFaroImage,
   },
   {
     id: 4,
+    name: "Casa J10",
+    slug: "casa-j10",
+    image: CasaJ10Image,
+  },
+  {
+    id: 5,
     name: "Casa de Praia",
     slug: "casa-de-praia",
     image: "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWV8ZW58MHx8MHx8fDA%3D",
   },
   {
-    id: 5,
+    id: 6,
     name: "Apartamento Minimalista",
     slug: "apartamento-minimalista",
     image: "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWV8ZW58MHx8MHx8fDA%3D",
   },
   {
-    id: 6,
+    id: 7,
     name: "Escritório Moderno",
     slug: "escritorio-moderno",
     image: "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGZyZWV8ZW58MHx8MHx8fDA%3D",
