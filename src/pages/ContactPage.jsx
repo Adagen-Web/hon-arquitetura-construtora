67b787a781a9d9d2aaf6538753519b80f2cdaf7a
Mateus Paulart <mateusp23@rede.ulbra.br>
2025-02-28 16:37:23 -0300

    ajuste pag contato

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,8 +6,8 @@ const ContactPage = () => {
     <section className="container mx-auto py-16 px-6">
       {/* Texto principal no topo */}
       <div className="mb-10 lg:ml-[17px] md:ml-[17px]">
-        <h2 className="text-4xl sm:text-4xl md:text-5xl lg:text-7xl font-yantramanav font-extralight text-primary leading-[1.1] text-left">
-          Fale com a HON <br /> e transforme ideias <br /> em projetos reais.
+        <h2 className="text-3xl sm:text-3xl text-[#050505] md:text-5xl lg:text-7xl font-yantramanav font-extralight text-primary leading-[1] text-left">
+          Faça seu projeto<br /> dos sonhos conosco!
         </h2>
       </div>
 
